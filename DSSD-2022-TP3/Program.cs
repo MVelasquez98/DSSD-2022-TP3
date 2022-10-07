763dd30cdcb5fc09916cf36456840b0c8c776135
Sol Cardozo <43156596+MVelasquez98@users.noreply.github.com>
2022-10-07 15:15:09 -0300

    cambios en swagger

--- DSSD-2022-TP3/Program.cs
+++ DSSD-2022-TP3/Program.cs
@@ -1,5 +1,7 @@
 using DSSD_2022_TP3;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,16 +18,20 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AcademicaContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => { c.EnableAnnotations(); });
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Gestion Academica", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Api V1");
+});
 
 app.UseHttpsRedirection();
 
